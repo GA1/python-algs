8c3a20bdbccb1f715892eb8055cecdc6d6a6cbb9
GA1 <nvkga1@gmail.com>
2017-06-30 22:54:59 +0200

    Don't use reserved words for variables

--- src/pe/pe_0001.py
+++ src/pe/pe_0001.py
@@ -2,13 +2,13 @@
 # The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
-def solve(N):
 
-    sum = 0
-    for i in range(1, N):
-        if i % 3 == 0 or  i % 5 == 0:
-            sum += i
-    return sum
+def solve(n):
+    result = 0
+    for i in range(1, n):
+        if i % 3 == 0 or i % 5 == 0:
+            result += i
+    return result
 
 
 if __name__ == "__main__":
