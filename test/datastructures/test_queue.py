166bd6ab6a42e2a7f435f97058b7435cda62d14a
GA1 <nvkga1@gmail.com>
2017-07-03 23:20:16 +0200

    Start queue

---
+++ test/datastructures/test_queue.py
@@ -0,0 +1,22 @@
+import unittest
+
+from src.datasctructures.queue import Queue
+from src.pe.pe_0001 import solve
+
+
+class MyTest(unittest.TestCase):
+
+    def test_empty_is_empty(self):
+        q = Queue()
+        self.assertTrue(q.is_empty())
+
+    def test_with_one_item_is_not_empty(self):
+        q = Queue()
+        q.enqueue(3)
+        self.assertFalse(q.is_empty())
+
+
+
+
+
+
